bf3ed4f4009a7580febdc11b43d435b81824feb4
Al-amin Ahmad Abbas <alaminahmadabbas@gmail.com>
2023-01-04 08:00:45 +0100

    select all neede dom elements

---
+++ lms/static/script/quiz.js
@@ -0,0 +1,7 @@
+const hambuger = document.querySelector(".hambuger");
+const questionGroup = document.querySelector(".question-group");
+const questionNumbers = document.querySelectorAll(".question-no");
+const questions = document.querySelectorAll(".quiz-content");
+const questionNavButton = document.querySelectorAll(".nav-button");
+const options = document.querySelectorAll(".quiz-options");
+const submit = document.querySelector(".submit-button");
