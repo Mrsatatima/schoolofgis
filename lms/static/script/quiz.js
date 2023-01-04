b14e03211423f37a5320a7c98e22f9c53c602fea
Al-amin Ahmad Abbas <alaminahmadabbas@gmail.com>
2023-01-04 08:18:58 +0100

    Add navication functions

--- lms/static/script/quiz.js
+++ lms/static/script/quiz.js
@@ -6,15 +6,39 @@ const questionNavButton = document.querySelectorAll(".nav-button");
 const options = document.querySelectorAll(".quiz-options");
 const submit = document.querySelector(".submit-button");
 
+let currentQuestion = 0;
+
 const minimizeQuestionNav = () => {
     hambuger.classList.toggle("active");
     questionGroup.classList.toggle("active");
   };
 
+  const removeActive = () => {
+    for (const number of questionNumbers) {
+      number.classList.remove("active");
+    }
+    for (const question of questions) {
+      question.classList.remove("active");
+    }
+  };
+  const toggleActive = (indx) => {
+    questions[indx].classList.toggle("active");
+    questionNumbers[indx].classList.toggle("active");
+    questionNumbers[indx].scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "start",
+    });
+  };
+
+  
 
 
 
 
 
+
+questions[currentQuestion].classList.toggle("active");
+questionNumbers[currentQuestion].classList.toggle("active");
 
 hambuger.addEventListener("click", minimizeQuestionNav);
