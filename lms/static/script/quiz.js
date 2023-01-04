934bd1cf6008f1a3b1435d31cad9b81cc191bbff
Al-amin Ahmad Abbas <alaminahmadabbas@gmail.com>
2023-01-04 08:24:52 +0100

    Add disable navigation

--- lms/static/script/quiz.js
+++ lms/static/script/quiz.js
@@ -9,100 +9,97 @@ const submit = document.querySelector(".submit-button");
 let currentQuestion = 0;
 
 const minimizeQuestionNav = () => {
-    hambuger.classList.toggle("active");
-    questionGroup.classList.toggle("active");
-  };
+  hambuger.classList.toggle("active");
+  questionGroup.classList.toggle("active");
+};
 
-  const removeActive = () => {
-    for (const number of questionNumbers) {
-      number.classList.remove("active");
-    }
-    for (const question of questions) {
-      question.classList.remove("active");
-    }
-  };
-  const toggleActive = (indx) => {
-    questions[indx].classList.toggle("active");
-    questionNumbers[indx].classList.toggle("active");
-    questionNumbers[indx].scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "start",
-    });
-  };
-
-  const disableNav = () =>{
-    if (currentQuestion=== 0 && questionNavButton[0].textContent =="Prev") {
-      questionNavButton[0].classList.add("pointer");
-     }
-     if(currentQuestion !== 0 && questionNavButton[0].textContent =="Prev") {
-      questionNavButton[0].classList.remove("pointer"); 
-     }
-     if (currentQuestion=== questions.length-1 && questionNavButton[1].textContent =="Next"){
-      questionNavButton[1].classList.add("pointer"); 
-     }
-     if (currentQuestion!== questions.length-1 && questionNavButton[1].textContent =="Next") {
-      questionNavButton[1].classList.remove("pointer");
-     }
+const removeActive = () => {
+  for (const number of questionNumbers) {
+    number.classList.remove("active");
   }
-  
-  const makeIndexWithinRange = (num) => {
-    if (num < 0) {
-      return 0;
-    } else if (num > questions.length - 1) {
-      return questions.length - 1;
-    }
-    return num;
-  };
-  
-  const addAsweredQuestioncolor = (answers, indx) => {
-    for (const answer of answers.children) {
-      if (answer.children[0].checked) {
-        questionNumbers[indx].classList.add("answered");
-        break;
-      } else {
-        questionNumbers[indx].classList.remove("answered");
-      }
-    }
-  };
-  
-  const selectQuestion = (indx) => {
-    addAsweredQuestioncolor(
-      questions[currentQuestion].children[1],
-      currentQuestion
-    );
-    if (indx !== currentQuestion) {
-      removeActive();
-      if (indx <= questions.length - 1) {
-        toggleActive(indx);
-      } else {
-        questionNumbers[indx].classList.toggle("active");
-      }
-      currentQuestion = indx;
-    disableNav()
-  
-    }
-  };
-  
-  const navigateQuestions = (event) => {
-    if (event.target.textContent === "Prev") {
-      indx = makeIndexWithinRange(currentQuestion - 1);
-      
+  for (const question of questions) {
+    question.classList.remove("active");
+  }
+};
+const toggleActive = (indx) => {
+  questions[indx].classList.toggle("active");
+  questionNumbers[indx].classList.toggle("active");
+  questionNumbers[indx].scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+    inline: "start",
+  });
+};
+
+const disableNav = () => {
+  if (currentQuestion === 0 && questionNavButton[0].textContent == "Prev") {
+    questionNavButton[0].classList.add("pointer");
+  }
+  if (currentQuestion !== 0 && questionNavButton[0].textContent == "Prev") {
+    questionNavButton[0].classList.remove("pointer");
+  }
+  if (
+    currentQuestion === questions.length - 1 &&
+    questionNavButton[1].textContent == "Next"
+  ) {
+    questionNavButton[1].classList.add("pointer");
+  }
+  if (
+    currentQuestion !== questions.length - 1 &&
+    questionNavButton[1].textContent == "Next"
+  ) {
+    questionNavButton[1].classList.remove("pointer");
+  }
+};
+
+const makeIndexWithinRange = (num) => {
+  if (num < 0) {
+    return 0;
+  } else if (num > questions.length - 1) {
+    return questions.length - 1;
+  }
+  return num;
+};
+
+const addAsweredQuestioncolor = (answers, indx) => {
+  for (const answer of answers.children) {
+    if (answer.children[0].checked) {
+      questionNumbers[indx].classList.add("answered");
+      break;
     } else {
-      indx = makeIndexWithinRange(currentQuestion + 1);
-  
+      questionNumbers[indx].classList.remove("answered");
     }
-    selectQuestion(indx);
-  };
+  }
+};
 
+const selectQuestion = (indx) => {
+  addAsweredQuestioncolor(
+    questions[currentQuestion].children[1],
+    currentQuestion
+  );
+  if (indx !== currentQuestion) {
+    removeActive();
+    if (indx <= questions.length - 1) {
+      toggleActive(indx);
+    } else {
+      questionNumbers[indx].classList.toggle("active");
+    }
+    currentQuestion = indx;
+    disableNav();
+  }
+};
 
-
-
-
-
-
+const navigateQuestions = (event) => {
+  if (event.target.textContent === "Prev") {
+    indx = makeIndexWithinRange(currentQuestion - 1);
+  } else {
+    indx = makeIndexWithinRange(currentQuestion + 1);
+  }
+  selectQuestion(indx);
+};
 
 questions[currentQuestion].classList.toggle("active");
 questionNumbers[currentQuestion].classList.toggle("active");
+disableNav()
 
 hambuger.addEventListener("click", minimizeQuestionNav);
